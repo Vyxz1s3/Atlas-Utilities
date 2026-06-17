754b3e43ee19306a365625fc331355cf2a5f9fde
Vyxz1s3 <clymansjerni@gmail.com>
2026-06-17 21:09:52 +0200

    Delete welcome.js

--- welcome.js
+++
@@ -1,7 +0,0 @@
-client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.get('YOUR_CHANNEL_ID');
-
-    if (!channel) return;
-
-    channel.send(`Welcome ${member} to the server! 🎉`);
-});
